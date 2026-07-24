f3cccc09ed6e92efca2ccb3a5572d1d3e3414681
Admin Second Chance Dog Rescue <tai@secondchancedogrescue.org>
2026-07-23 21:58:11 -0700

    Update Header.jsx logo was overwritten

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import '../css/header.css';
+import logo from '../assets/SCDR_logo.png';
 import { navItems } from '../data/navigation';
 
 const Header = () => {
