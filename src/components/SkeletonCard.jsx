890e7770505637f253fd192b269add7a4829b668
Tai〽️ <taitran@sandiego.edu>
2026-07-30 15:52:01 -0700

    Initial

---
+++ src/components/SkeletonCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css'
+
+const SkeletonCard = ({ className = '' }) => {
+    return (
+        <div className="tw-h-[428px] tw-w-full tw-relative">
+            <div className='tw-w-[300px] tw-h-full'>
+                <Skeleton height='100%' />
+            </div>
+
+            <div className="tw-absolute tw-bottom-0 tw-left-0 tw-right-0 tw-p-3 tw-bg-white/90">
+                <Skeleton width={140} height={28} />
+                <Skeleton width={70} />
+                <Skeleton width={120} />
+                <Skeleton width={90} />
+                <Skeleton width={120} height={36} borderRadius={9999} />
+            </div>
+        </div>
+    )
+}
+
+export default SkeletonCard
