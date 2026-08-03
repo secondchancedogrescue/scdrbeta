796116a9bc61729abec3b8838e8a294fe0d04e82
Tai〽️ <taitran@sandiego.edu>
2026-08-03 00:08:13 -0700

    Update adoption fee link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -46,10 +46,9 @@ const Footer = () => {
           <a className='tw-mt-2 tw-px-1 tw-py-1 hover:tw-text-[#cc0000] hover:tw-bg-white' href="https://2ndchancedogrescue.substack.com" target='_blank' rel='noopener noreferrer'>
             Our Substack
           </a>
-          <a className='tw-mt-2 tw-px-1 tw-py-1 hover:tw-text-[#cc0000] hover:tw-bg-white'
-            href="https://www.paypal.com/donate/?hosted_button_id=XPTRRJU3PLLPN" target="_blank" rel="noopener noreferrer">
+          <Link to='/fee' onClick={scrollToTop} className='tw-mt-2 tw-px-1 tw-py-1 hover:tw-text-[#cc0000] hover:tw-bg-white'>
             Adoption Fee
-          </a>
+          </Link>
           <Link to='/privacy' onClick={scrollToTop}
             className='tw-mt-2 tw-px-1 tw-py-1 hover:tw-text-[#cc0000] hover:tw-bg-white'>
             Privacy Policy
