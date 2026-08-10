fbd7330ed44713eee0c8a66fead6ae4146ee3b77
Tai〽️ <taitran@sandiego.edu>
2026-08-09 20:49:35 -0700

    Move Adoption Fee to nav

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -1,6 +1,7 @@
 export const navItems = [
   { to: "/", label: "Home" },
   { to: "/dogs", label: "Available Dogs" },
+  { to: "/fee", label: "Adoption Fee" },
   { to: "/volunteer", label: "Volunteer" },
   { to: "/us", label: "About Us" },
   { to: "/donate", label: "Donate" },
